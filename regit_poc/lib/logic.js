933c7321c074f4b9c4cce2068255be5a54238029
barkvox <joshua.a.barker@gmail.com>
2017-12-05 15:09:30 +0700

    edit

--- regit_poc/lib/logic.js
+++ regit_poc/lib/logic.js
@@ -1,23 +1,90 @@
-'use strict';
-/**
- * Write your transction processor functions here
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+'use strict';
+
 /**
- * Sample transaction
- * @param {org.regit.network.ChangeAssetValue} changeAssetValue
+ * A Member grants access to their record to another Member.
+ * @param {regit.transactions.AuthorizeAccess} authorize - the authorize to be processed
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.regit.network.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
+function authorizeAccess(authorize) {
+
+    var me = getCurrentParticipant();
+    console.log('**** AUTH: ' + me.getIdentifier() + ' granting access to ' + authorize.memberId );
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+
+    // if the member is not already authorized, we authorize them
+    var index = -1;
+
+    if(!me.authorized) {
+        me.authorized = [];
+    }
+    else {
+        index = me.authorized.indexOf(authorize.memberId);
+    }
+
+    if(index < 0) {
+        me.authorized.push(authorize.memberId);
+
+        return getParticipantRegistry('regit.transactions.Member')
+        .then(function (memberRegistry) {
+
+            // emit an event
+            var event = getFactory().newEvent('regit.transactions', 'MemberEvent');
+            event.memberTransaction = authorize;
+            emit(event);
+
+            // persist the state of the member
+            return memberRegistry.update(me);
         });
+    }
+}
+
+/**
+ * A Member revokes access to their record from another Member.
+ * @param {regit.transactions.RevokeAccess} revoke - the RevokeAccess to be processed
+ * @transaction
+ */
+function revokeAccess(revoke) {
+
+    var me = getCurrentParticipant();
+    console.log('**** REVOKE: ' + me.getIdentifier() + ' revoking access to ' + revoke.memberId );
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+
+    // if the member is authorized, we remove them
+    var index = me.authorized ? me.authorized.indexOf(revoke.memberId) : -1;
+
+    if(index>-1) {
+        me.authorized.splice(index, 1);
+
+        return getParticipantRegistry('regit.transactions.Member')
+        .then(function (memberRegistry) {
+
+            // emit an event
+            var event = getFactory().newEvent('regit.transactions', 'MemberEvent');
+            event.memberTransaction = revoke;
+            emit(event);
+
+            // persist the state of the member
+            return memberRegistry.update(me);
+        });
+    }
 }
